33619e7158dbb6512aaeb13b46874f38fd0bb634
Mark Thompson <mthompson@bitovi.com>
2022-11-10 23:32:30 -0800

    Initial commit

---
+++ e2e/application-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'application-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/e2e/application-e2e',
+};
